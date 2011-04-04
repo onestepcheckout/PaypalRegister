ab1bbdc84f6c17b69a201288a77514d020b06a7a
Anton Siniorg <anton@lahendus.ee>
2011-04-04 12:06:53 +0200

    fix: added firstname, lastname checks , improved code commenting

--- app/code/local/Idev/PaypalRegister/Model/Observer.php
+++ app/code/local/Idev/PaypalRegister/Model/Observer.php
@@ -2,18 +2,27 @@
 class Idev_PaypalRegister_Model_Observer extends Varien_Object
 {
 
+    /**
+     *
+     * simple instance counter variable
+     * @var int
+     */
     public $count = 0;
 
     public function _construct(){
+
         parent::_construct();
+
+        //currently only paypal_express is tested with this but can be any other method
         $this->setPaypalMethods(array('paypal_express'));
+
         $config = new Varien_Object(Mage::getStoreConfig('paypalregister', Mage::app()->getStore()));
         $this->setConfig($config);
     }
 
     /**
      *
-     * Manipulate the quote customer object before it is saved
+     * Manipulate the quote and customer object before it is saved and change the method to register
      * @param object $observer
      */
     public function salesOrderBeforeSave($observer)
@@ -42,6 +51,17 @@ class Idev_PaypalRegister_Model_Observer extends Varien_Object
                 //prepare customer in quote
                 $this->getCheckout()->prepareNewCustomerQuote();
 
+                //dependant of what customer information paypal sends back we need to set some data from billing address data (e-mail is always sent)
+                if(!$this->getQuote()->getCustomerFirstname() || !$this->getQuote()->getCustomer()->getFirstname()){
+                    $this->getQuote()->getCustomer()->setFirstname($this->getQuote()->getBillingAddress()->getFirstname());
+                    $this->getQuote()->setCustomerFirstname($this->getQuote()->getBillingAddress()->getFirstname());
+                }
+
+                if(!$this->getQuote()->getCustomerLastname() || !$this->getQuote()->getCustomer()->getLastname()){
+                    $this->getQuote()->getCustomer()->setLastname($this->getQuote()->getBillingAddress()->getLastname());
+                    $this->getQuote()->setCustomerLastname($this->getQuote()->getBillingAddress()->getLastname());
+                }
+
             }
             return $this;
         }
@@ -54,8 +74,14 @@ class Idev_PaypalRegister_Model_Observer extends Varien_Object
         return Mage::getSingleton('paypalregister/checkout_type_onepage');
     }
 
+    /**
+     *
+     * Send out new customer notification e-mail if you are dealing with new customer
+     * @param object $observer
+     */
     public function saveOrderAfter($observer){
 
+        //its useful to check if actual methods with output (like e-mail) are called only once if bound to events
         if($this->count == 0){
 
             if($this->isEnabled()){
@@ -77,6 +103,12 @@ class Idev_PaypalRegister_Model_Observer extends Varien_Object
         }
     }
 
+    /**
+     * Check if the methods are allowed for current payment method
+     *
+     * @param string $paymentMethod
+     * @return bool
+     */
     public function isAllowedMethod($paymentMethod = false){
         $paymentMethods = $this->getPaypalMethods();
         if($paymentMethod && !empty($paymentMethods) && in_array($paymentMethod, $paymentMethods)){
@@ -89,7 +121,7 @@ class Idev_PaypalRegister_Model_Observer extends Varien_Object
     /**
      * Check if this method is enabled
      *
-     * $return bool
+     * @return bool
      */
     public function isEnabled(){
         if(!$this->isLoggedIn() && $this->getConfig()->getGeneral('enable_paypalregister')){
@@ -100,7 +132,7 @@ class Idev_PaypalRegister_Model_Observer extends Varien_Object
     }
 
     /**
-     * Check customer is logged in
+     * Check if customer is logged in
      *
      * @return bool
      */
